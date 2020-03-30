64bb7d5583028d9b1561004fec209316ce41d972
vswaminathanbrierley <vswaminathan@brierley.com>
2020-03-30 16:01:50 -0500

    Automation for GetMemberRewardSummarybyId

--- API/Controllers/MemberController.cs
+++ API/Controllers/MemberController.cs
@@ -186,6 +186,31 @@ namespace Hertz.API.Controllers
                 }
             }
         }
+        public MemberRewardSummaryModel GetMemberRewardSummaryById(long memberRewardId, string language, string programCode, string externalId)
+        {
+            using (ConsoleCapture capture = new ConsoleCapture())
+            {
+                MemberRewardSummaryModel memberRewardsModelOut = default;
+                try
+                {
+                    var lwOut = lwSvc.GetMemberRewardSummaryById(memberRewardId, language, programCode, externalId, out double time);
+                    memberRewardsModelOut = LODConvert.FromLW<MemberRewardSummaryModel>(lwOut.MemberRewardSummary);
+                    return memberRewardsModelOut;
+                }
+                catch (LWClientException ex)
+                {
+                    throw new LWServiceException(ex.Message, ex.ErrorCode);
+                }
+                catch (Exception ex)
+                {
+                    throw new LWServiceException(ex.Message, -1);
+                }
+                finally
+                {
+                    stepContext.AddAttachment(new Attachment("GetMemberRewardSummaryById", capture.Output, Attachment.Type.Text));
+                }
+            }
+        }
         public AwardLoyaltyCurrencyResponseModel AwardLoyaltyCurrency(string loyaltyID, decimal points)
         {
             using (ConsoleCapture capture = new ConsoleCapture())
@@ -380,7 +405,14 @@ select lm.*
             return dbContext.Query<MemberRewardsModel>(query.ToString());
         }
 
+        public IEnumerable<MemberRewardSummaryModel> GetMemberRewardsummaryFromDB(decimal? ipcode, long? memberrewardid)
+        {
+            string query = $@"select rd.howmanypointstoearn as POINTSCONSUMED, rd.name as REWARDNAME, mr.dateissued, mr.expiration from BP_HTZ.Lw_Memberrewards mr 
+                                inner join BP_HTZ.Lw_Rewardsdef rd on rd.id = mr.rewarddefid
+                                where mr.ID = {memberrewardid} and mr.memberid = {ipcode}";
 
+            return dbContext.Query<MemberRewardSummaryModel>(query.ToString());
+        }
 
         public decimal GetPointSumFromDB(string loyaltyid)
         {
---
+++ API/DataModels/MemberRewardSummaryModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using Brierley.TestAutomation.Core.Utilities;
+using Brierley.TestAutomation.Core.SFTP;
+
+namespace Hertz.API.DataModels
+{
+    public class MemberRewardSummaryModel
+    {
+        public decimal MEMBERID { get; set; }
+        [ModelAttribute("Pointsconsumed", ReportOption.Print)]
+        public decimal POINTSCONSUMED { get; set; }
+        [ModelAttribute("CertificateNmbr", ReportOption.Print)]
+        public string CERTIFICATENMBR { get; set; }
+        [ModelAttribute("AvailableBalance", ReportOption.Print)]
+        public decimal AVAILABLEBALANCE { get; set; }
+        [DateTimeCompare(TimeCompare.Day | TimeCompare.Month | TimeCompare.Year)]
+        [ModelAttribute("DateIssued", ReportOption.Print)]
+        public DateTime? DATEISSUED { get; set; }
+        [DateTimeCompare(TimeCompare.Day | TimeCompare.Month | TimeCompare.Year)]
+        [ModelAttribute("ExpirationDate", ReportOption.Print)]
+        public DateTime? EXPIRATION { get; set; }
+        public DateTime? FULFILLMENTDATE { get; set; }
+        public DateTime? REDEMPTIONDATE { get; set; }
+        [ModelAttribute("RewardName", ReportOption.Print)]
+        public string REWARDNAME { get; set; }
+        public string CATEGORYNAME { get; set; }
+        public string SHORTDESCRIPTION { get; set; }
+        public string LONGDESCRIPTION { get; set; }
+        public string LEGALTEXT { get; set; }
+        [ModelAttribute("smallImageFile", ReportOption.Print)]
+        public string SMALLIMAGEFILE { get; set; }
+        public string MEMBERIDENTITY { get; set; }
+    }
+}
