d795352c0a64f74e1987b54110430cbe180ff558
Esteban <ebarron@brierley.com>
2020-04-13 09:47:35 -0500

    HTZ-22510 changes

---
+++ API/DataModels/HertzGetAccountActivitySummaryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using Brierley.TestAutomation.Core.Utilities;
+using Brierley.TestAutomation.Core.SFTP;
+
+namespace Hertz.API.DataModels
+{
+    public class HertzGetAccountActivitySummaryModel
+    {
+        public const string TABLENAME = null;
+        [DateTimeCompare(TimeCompare.Day | TimeCompare.Month | TimeCompare.Year)]
+        [ModelAttribute("ActivityDate", ReportOption.Print)]
+        public DateTime? ACTIVITYDATE { get; set; }
+        public string TYPE { get; set; }
+        public string DESCRIPTION { get; set; }
+        public string RANUMBER { get; set; }
+        public int POINTS { get; set; }
+        public int DISPOSITIONCODE { get; set; }
+        public string DISPOSITIONCODEDESCRIPTION { get; set; }
+        public List<PointsDetailModel> pointDetails { get; set; }
+
+
+    }
+}
---
+++ API/DataModels/PointsDetailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hertz.API.DataModels
+{
+    public class PointsDetailModel
+    {
+            public DateTime? DATE { get; set; }
+            public int POINTS { get; set; }
+            public string DESCRIPTION { get; set; }
+            public string RANUMBER { get; set; }
+            public string TYPE { get; set; }
+            public string NOTES { get; set; }
+    }
+}
